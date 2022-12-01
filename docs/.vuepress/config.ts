4ef5b306fc1b68ebb2fd9bafd4b8efb210ccf96e
fuchen <284992984@qq.com>
2022-12-01 13:32:44 +0800

    nginx

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,14 +3,15 @@ import { defaultTheme } from '@vuepress/theme-default'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
+  title: '爬坑记录',
+  description: '记录爬的每个坑',
   theme: defaultTheme({
     navbar: [
       {
         text: 'VUE',
         link: '/vue'
       },
+
       {
         text: '关于',
         children: [
